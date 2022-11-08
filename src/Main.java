6bf27bd58a6689f48a096b6de2ed5a444ddd8a95
whmnet <whmnet@yahoo.com.br>
2022-11-08 11:51:15 -0300

    aula variaveis primeira versão

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+/* Exercicio 1 - Tipos de variáveis, dados e operadores aritméticos  */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int _1a;
+        int $aq;
+        int asrn24678md;
+        int asrn$2478_md;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+        final int j = 10;
+        asrn24678md = 100;
+        asrn$2478_md = 10;
+
+        int quantidadeProduto = 50;
+        final int NUMERO_TENTATIVAS = 10;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn$2478_md);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+
+    }
+}
